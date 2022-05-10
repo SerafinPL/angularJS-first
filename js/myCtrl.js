b19fa5cf462efa07aa20e2d93753b440334769f7
serafinpl <ser.burberibar@gmail.com>
2022-05-10 22:58:19 +0200

    changes

--- js/myCtrl.js
+++ js/myCtrl.js
@@ -1,11 +1,19 @@
 
 app.controller("myCtrl", function($scope) {
-    $scope.firstName = "John";
-    $scope.lastName = "Doe";
+    // $scope.firstName = "John";
+    // $scope.lastName = "Doe";
 
-    $scope.getName = function() {
-      return($scope.firstName + ' ' + $scope.lastName);  
-    }
+    // $scope.getName = function() {
+    //   return($scope.firstName + ' ' + $scope.lastName);  
+    // }
 
-    console.log($scope.getName());
+    // console.log($scope.getName());
+
+
   });
+
+  const searchPeople = function (firstName, lastName, height, age){
+    return 'zwrot funkcji'
+  };
+
+  console.log(searchPeople);
