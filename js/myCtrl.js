5870000160051e9511f8ede540decdf9d49bca49
serafinpl <ser.burberibar@gmail.com>
2022-05-12 21:20:58 +0200

    new features

--- js/myCtrl.js
+++ js/myCtrl.js
@@ -1,14 +1,19 @@
 
-app.controller("myCtrl", function($scope) {
-    // $scope.firstName = "John";
-    // $scope.lastName = "Doe";
-
+app.controller("myCtrl", function($scope, $log, $filter) {
+    $scope.firstName = "John";
+    
+    $scope.lastName = "Doe";
+    $scope.formatedName = $filter('uppercase')($scope.firstName)
     // $scope.getName = function() {
     //   return($scope.firstName + ' ' + $scope.lastName);  
     // }
 
     // console.log($scope.getName());
-
+    $log.log($scope.firstName);
+    $log.info($scope.formatedName);
+    $log.warn('ostrzerzenie');
+    $log.debug('debug??');
+    $log.error('BŁĄD niekrytyczny');
 
   });
 
