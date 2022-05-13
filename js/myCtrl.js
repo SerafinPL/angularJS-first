8d98f12eab2a0a64227b9e07e8a23d96d6e5a6b3
serafinpl <ser.burberibar@gmail.com>
2022-05-13 21:30:58 +0200

    next steps

--- js/myCtrl.js
+++ js/myCtrl.js
@@ -1,24 +1,43 @@
 
-app.controller("myCtrl", function($scope, $log, $filter) {
-    $scope.firstName = "John";
+// app.controller("myCtrl", function($scope, $log, $filter) {
+//     $scope.firstName = "John";
     
-    $scope.lastName = "Doe";
-    $scope.formatedName = $filter('uppercase')($scope.firstName)
-    // $scope.getName = function() {
-    //   return($scope.firstName + ' ' + $scope.lastName);  
-    // }
+//     $scope.lastName = "Doe";
+//     $scope.formatedName = $filter('uppercase')($scope.firstName)
+//     // $scope.getName = function() {
+//     //   return($scope.firstName + ' ' + $scope.lastName);  
+//     // }
 
-    // console.log($scope.getName());
-    $log.log($scope.firstName);
-    $log.info($scope.formatedName);
-    $log.warn('ostrzerzenie');
-    $log.debug('debug??');
-    $log.error('BŁĄD niekrytyczny');
+//     // console.log($scope.getName());
+//     $log.log($scope.firstName);
+//     $log.info($scope.formatedName);
+//     $log.warn('ostrzerzenie');
+//     $log.debug('debug??');
+//     $log.error('BŁĄD niekrytyczny');
 
-  });
+//   });
 
-  const searchPeople = function (firstName, lastName, height, age){
-    return 'zwrot funkcji'
-  };
+//   const searchPeople = function (firstName, lastName, height, age){
+//     return 'zwrot funkcji'
+//   };
 
-  console.log(searchPeople);
+//   console.log(searchPeople);
+
+
+app.controller('myCtrl',['$scope', '$log', '$timeout','$filter', function($scope, $log, $timeout, $filter){
+
+  $log.info($scope)
+
+  $timeout(function() {
+    $scope.time = new Date;
+  },3000);
+
+  $scope.handle = '';
+
+  $scope.lowerCaseFunc = function () {
+
+    return $filter('lowercase')($scope.handle);
+  }
+
+
+}])
