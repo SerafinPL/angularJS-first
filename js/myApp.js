18c37ebd1f67b7eee006ca264e1206072963a31d
serafinpl <ser.burberibar@gmail.com>
2022-05-19 19:41:58 +0200

    start outing

--- js/myApp.js
+++ js/myApp.js
@@ -1,16 +1,46 @@
 var app = angular.module("myApp", ['ngMessages', "ui.router"]); 
 
+
+
+app.component('hello', {
+    template:  '<h3>{{greeting}} Solar System!</h3>' +
+               '<button ng-click="toggleGreeting()">toggle greeting</button>',
+             
+    controller: function($scope) {
+      $scope.greeting = 'hello';
+    
+      $scope.toggleGreeting = function() {
+        $scope.greeting = ($scope.greeting == 'hello') ? 'whats up' : 'hello'
+      }
+    }
+  })
+
+app.component('about', {
+    template:   '<h3> Sekcja About !</h3>' +
+                '<p>{{clickDate}}</p>' +
+                '<button ng-click="click()">Kliknij po date</button>',
+    controller: function($scope){
+
+        $scope.clickDate = 'brak daty'
+        $scope.click = function(){
+            this.clickDate = new Date();
+        }
+    }
+})
+
 app.config(function($stateProvider) {
     var helloState = {
       name: 'hello',
       url: '/hello',
-      template: '<h3>hello world!</h3>'
+      component: 'hello'
+    //   template: '<h3>hello world!</h3>'
     }
   
     var aboutState = {
       name: 'about',
       url: '/about',
-      template: '<h3>Its the UI-Router hello world app!</h3>'
+      component: 'about'
+    //   template: '<h3>Its the UI-Router hello world app!</h3>'
     }
   
     $stateProvider.state(helloState);
